fc68142a92ff70094d581ef60fc76f3e9e0c01c7
thiagolima86 <thiagolima86@gmail.com>
2013-10-18 11:15:38 -0300

    Create ajaxform.1.0.js

---
+++ ajaxform.1.0.js
@@ -0,0 +1,19 @@
+jQuery.fn.ajaxForm = function (callback) {
+
+//var options = $.extend()
+
+  $(this).submit(function(){
+    var serial = $(this).serialize();
+    var action = $(this).attr("action");
+    var method = $(this).attr("method").toUpperCase();
+
+    $.ajax({
+            type: method,
+            url: action,
+            data: serial,
+            success: callback           
+        });
+    $(this).each (function() { this.reset(); });
+    return false;
+    });
+}
